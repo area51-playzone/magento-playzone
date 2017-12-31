1e534d529bff800789d45d4a3b4153af596f01a5
Syed Ishtiaque Ahmad <ishtiaque05@gmail.com>
2017-12-31 14:59:47 +0600

    Database schema install and upgrade - script to create table - seed install data - upgrade schema to change existing database columns and columns type - upgrade seed data to seed new value every time setup:upgrade is ran - Uninstall script to drop database

--- app/code/Mageplaza/HelloWorld/Controller/Index/Test.php
+++ app/code/Mageplaza/HelloWorld/Controller/Index/Test.php
@@ -15,7 +15,10 @@ class Test extends \Magento\Framework\App\Action\Action
 
 	public function execute()
 	{
-		echo "hello worlds";
+		$textDisplay = new \Magento\Framework\DataObject(array('text' => 'Mageplaza'));
+		$this->_eventManager->dispatch('mageplaza_helloworld_display_text', ['mp_text' => $textDisplay]);
+		echo $textDisplay->getText();
 		exit;
+
 	}
 }
